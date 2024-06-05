45a432b7f5c8edc297cc4dafc0790ffdfa7f6ce8
Valentin Heiderich <valentin.heiderich@gmx.de>
2024-06-05 20:53:25 +0200

    Fixed link-card UI elements took time to disappear.

--- public/script.js
+++ public/script.js
@@ -3,8 +3,10 @@ const socket = io();
 let links_btn = document.getElementById("links_btn");
 let home_btn = document.getElementById("home_btn");
 let container = document.getElementById("container");
-function change_visibility(element) {element.style.visibility = element.style.visibility === "hidden" ? "visible" : "hidden";}
+function toggle_visibility(element) {element.style.visibility = element.style.visibility === "hidden" ? "visible" : "hidden";}
+function toggle_transition_time(group){for(let _child_i in group){if(_child_i === "length") break;let _child = group[_child_i]; _child.style.transition = _child.style.transition === "all 0.5s ease 0s" ? "0s" : "0.5s";console.log(_child.style.transition);}}
 links_btn.addEventListener("click", () => {container.style.visibility="visible";});
-home_btn.addEventListener("click", () => {container.style.visibility="hidden";});
-change_visibility(container);
+home_btn.addEventListener("click", () => {toggle_transition_time(container.children);container.style.visibility="hidden";setTimeout(() => {toggle_transition_time(container.children);}, 500);});
+toggle_visibility(container);
+toggle_transition_time(container.children);
 socket.on("users", (active_users) => {document.getElementById("active_users").innerHTML = active_users;});
